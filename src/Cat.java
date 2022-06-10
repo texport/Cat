fd09f7f8c8d935aec4d8bf56b39188b66b6a2352
Сергей Иванов <s.ivanov@iberica.kz>
2022-06-10 22:30:31 +0600

    v3

--- src/Cat.java
+++ src/Cat.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Cat
 {
     // Переменные
@@ -5,23 +7,53 @@ public class Cat
     private double weight;
     public static int count;
     private boolean isAlive;
+    private boolean hasTail;
     private double feedCount;
     private String deadMessage = "ДОХЛАЯ КОШКА НЕ МОЖЕТ КУШАТь, ХОДИТЬ В ТУАЛЕТ, МЯУКАТЬ!!!";
     private CatColor catColor1;
+
     // Константы
     private static final double MIN_WEIGHT = 1000.0;
     private static final double MAX_WEIGHT = 9000.0;
 
     private static final int EYE_COUNT = 2;
 
+    // Конструктор кошки
     public Cat()
     {
-        weight = 1500.0 + 3000.0 * Math.random(); // устанавливаем начальный вес кошке
+        //weight = 1500.0 + 3000.0 * Math.random(); // устанавливаем начальный вес кошке
         originWeight = weight;
         feedCount = 0; // счетчик еды для кошки
         isAlive = true; // статус кошки
+        hasTail = false; // хвост по умолчанию
         catColor1 = CatColor.NONE; // цвет кошки по умолчанию
         setCatCount(); // при создании объекта кошка считаем ее живой и добовляем в счетчик живых кошек
+        setTail(); // вызываем хвост
+    }
+
+    private void setTail()
+    {
+        Random random = new Random();
+        hasTail = random.nextBoolean();
+    }
+
+    public String getTail()
+    {
+        if (hasTail)
+        {
+            return "У меня есть хвост!";
+        }
+        else
+        {
+            return "У меня нет хвоста";
+        }
+    }
+
+    // Конструктор веса
+    public Cat (Double weight)
+    {
+        this();
+        this.weight = weight;
     }
 
     // Добавляем кошку
@@ -30,10 +62,16 @@ public class Cat
         count = count + 1;
     }
 
-    // Передаем цвет кошки
+    // Устанавлиеваем цвет кошки
     public void setCatColor (CatColor catColor)
     {
-        catColor1 = catColor;
+        this.catColor1 = catColor;
+    }
+
+    // Узнаем какой цвет у кошки
+    public CatColor getCatColor()
+    {
+        return catColor1;
     }
 
     // Кошка умерла
@@ -53,6 +91,7 @@ public class Cat
         return count;
     }
 
+    // Кошка мяукает
     public void meow()
     {
         if (isAlive)
@@ -69,7 +108,7 @@ public class Cat
     // Кормить кошку
     public void feed(Double amount)
     {
-        if (isAlive)
+        if(isAlive)
         {
             weight = weight + amount;
             feedCount = feedCount + amount;
@@ -83,7 +122,7 @@ public class Cat
     // Напоить кошку
     public void drink(Double amount)
     {
-        if (isAlive)
+        if(isAlive)
         {
             weight = weight + amount;
         }
@@ -96,7 +135,7 @@ public class Cat
     // Получить вес кошки
     public Double getWeight()
     {
-        if (isAlive)
+        if(isAlive)
         {
             return weight;
         }
@@ -107,7 +146,7 @@ public class Cat
         return null;
     }
 
-    //
+    // Общую сумму съеденой еды кошкой
     public Double getCountFeed()
     {
         return feedCount;
--- src/Loader.java
+++ src/Loader.java
@@ -5,7 +5,7 @@ public class Loader
     public static void main(String[] args)
     {
         // Cat1
-        Cat cat1 = new Cat();
+        Cat cat1 = getKitten(1500.0 + 3000.0 * Math.random());
         cat1.setCatColor(CatColor.BLACK); // цвет кошки при создании
         System.out.println("");
         System.out.println("Имя кошки " + cat1);
@@ -16,11 +16,12 @@ public class Loader
         System.out.println("Вес Cat1 теперь " + cat1.getWeight());
         System.out.println("-----------------------------");
         System.out.println("Кол-во кошек: " + Cat.getCatCount());
+        System.out.println(cat1.getTail());
         enterText();
         System.out.println("");
 
         // Cat2
-        Cat cat2 = new Cat();
+        Cat cat2 = getKitten(1500.0 + 3000.0 * Math.random());
         System.out.println("Имя кошки " + cat2);
         System.out.println("-----------------------------");
         System.out.println("Вес Cat2: " + cat2.getWeight());
@@ -29,11 +30,12 @@ public class Loader
         System.out.println("Вес Cat2 теперь: " + cat2.getWeight());
         System.out.println("-----------------------------");
         System.out.println("Кол-во кошек: " + Cat.getCatCount());
+        System.out.println(cat2.getTail());
         enterText();
         System.out.println("");
 
         // Cat3
-        Cat cat3 = new Cat();
+        Cat cat3 = getKitten(1500.0 + 3000.0 * Math.random());
         System.out.println("Имя кошки " + cat3);
         System.out.println("-----------------------------");
         System.out.println("Вес Cat3: " + cat3.getWeight());
@@ -44,11 +46,12 @@ public class Loader
         }
         System.out.println("-----------------------------");
         System.out.println("Кол-во кошек: " + Cat.getCatCount());
+        System.out.println(cat3.getTail());
         enterText();
         System.out.println("");
 
         // Cat 4
-        Cat cat4 = new Cat();
+        Cat cat4 = getKitten(1500.0 + 3000.0 * Math.random());
         System.out.println("Имя кошки " + cat4);
         System.out.println("-----------------------------");
         System.out.println("Вес Cat4: " + cat4.getWeight());
@@ -56,13 +59,14 @@ public class Loader
             cat4.meow();
             System.out.println(cat4.statusController() == "Dead" ? cat4.deadCat() : "Ваша кошка очень много кричит");
         }
+        System.out.println(cat4.getTail());
         System.out.println("-----------------------------");
         System.out.println("Кол-во кошек: " + Cat.getCatCount());
         enterText();
         System.out.println("");
 
         // Cat5
-        Cat cat5 = new Cat();
+        Cat cat5 = getKitten(1500.0 + 3000.0 * Math.random());
         System.out.println("Имя кошки " + cat5);
         System.out.println("-----------------------------");
         System.out.println("Вес Cat5: " + cat5.getWeight()
@@ -77,14 +81,46 @@ public class Loader
         System.out.println(cat5.getCountFeed());
         System.out.println("-----------------------------");
         System.out.println("Кол-во кошек: " + Cat.getCatCount());
+        System.out.println(cat5.getTail());
         enterText();
         System.out.println("");
+
+        // Cat6
+        Cat cat6 = getKitten(1100.0);
+        System.out.println("");
+        System.out.println("Имя кошки " + cat6);
+        System.out.println("-----------------------------");
+        System.out.println("Цвет кошки: " + cat6.getCatColor());
+        cat6.setCatColor(CatColor.ORANGE); // цвет кошки при создании
+        System.out.println("Цвет кошки: " + cat6.getCatColor());
+        System.out.println("Вес Cat6: " + cat6.getWeight());
+        System.out.print("Cat6 сказал: ");
+        cat6.meow();
+        System.out.println("Вес Cat6 теперь " + cat6.getWeight());
+        System.out.println(cat6.getTail());
+        System.out.println("-----------------------------");
+        System.out.println("Кол-во кошек: " + Cat.getCatCount());
+        enterText();
+        System.out.println("");
+
+        // Cat7
+        Cat cat7 = getKitten(1500.0 + 3000.0 * Math.random());
+        System.out.println(cat7.getTail());
     }
 
-    public static void enterText()
+    // Метод создающий кота
+    private static Cat getKitten(Double weight)
     {
+        Cat cat = new Cat(weight);
+        return cat;
+    }
+
+    public static String enterText()
+    {
+        System.out.println(" ");
+        System.out.println("Нажмите ENTER что бы продолжить...");
         Scanner scanner = new Scanner(System.in);
-        String title = scanner.nextLine();
-        System.out.println("Нажмите ENTER что бы продолжить: " + title);
+        String text = scanner.nextLine();
+        return text;
     }
 }
